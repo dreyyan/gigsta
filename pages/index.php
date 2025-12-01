cc0eedf28d94f96678b976a602757dda80d1fd99
denv-ae <denver.fermase@wvsu.edu.ph>
2025-12-01 23:56:05 +0800

    ADDED header and started hero section

--- pages/index.php
+++ pages/index.php
@@ -3,9 +3,117 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
-    
+
+    <header>
+        <!-- Logo -->
+        <div>
+            <img src="../images/gigsta-logo.svg"  >
+        </div>
+
+        <!-- Header Right Section-->
+        <div class="header-btns">
+            <!-- Explore & Become a Freelancer Btn-->
+            <div class="header-links">
+                <a> Explore </a>
+                <a> Become a Freelancer </a>
+            </div>
+
+            <!-- Signin & Join Btn-->
+            <div class="header-links">
+                <a> Sign In </a>
+                <a id="primary-btn"> Join </a>
+            </div>
+        </div>
+    </header>
+    <!-- Hero Section --->
+    <div class="gradient-divider"></div>
+    <main>
+        <!-- Hero title -->
+         <div class="hero-title"> 
+            <h1> 
+                Get Gigs Done at
+            </h1>
+            <h1 class="highlighted-title"> 
+                Lightning Speed
+            </h1> 
+         </div>
+
+        <!-- Hero description -->
+         <div> 
+            <h2> 
+                Fixed-price gigs delivered in hours, not weeks.
+            </h2>
+        </div> 
+
+        <!-- Hero Search Bar Section -->
+            
+        <div> 
+            <!-- Search Bar -->
+            <div>
+                <input type="search"/>
+                <button>Search</button>
+            </div>
+            
+            <!-- Quick Links -->
+            <div> 
+                <div> 
+                    Web Dev
+                </div>
+                <div>
+                    Archi & Interior Design 
+                <div> 
+                <div>
+                    Video Editing 
+                <div> 
+            </div>
+            
+        </div> 
+        
+        <!-- Hero ratings -->
+        <div>
+            RATINGS 
+        </div>
+
+        <!-- Hero 3 Columns -->
+
+        <div>
+            <div>
+                <div> 
+                    <img src="../images/temp.png">
+                    <h3>DELIVERY</h3>
+                </div>
+                <p> 
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, vel itaque ab ullam blanditiis ad incidunt tempore at facere commodi id, eaque inventore minus delectus debitis. Beatae blanditiis temporibus soluta!
+                </p>
+            </div>
+
+            <div>
+                <div> 
+                    <img src="../images/temp.png">
+                    <h3>LOCAL TALENT</h3>
+                </div>
+                <p> 
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae vero ab sapiente tenetur doloribus, voluptatibus fugit. Quam, exercitationem. Porro maiores nihil, alias ad quasi odit nemo voluptates recusandae officiis perferendis.
+                </p>
+            </div>
+
+            <div>
+                 <div> 
+                    <img src="../images/temp.png">
+                    <h3>PRICE</h3>
+                </div>
+                <p> 
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo, deserunt at? Quisquam animi voluptas maxime ab? Aut architecto at voluptatem quis ut eos nostrum ea, deleniti reiciendis laboriosam iusto quidem?
+                </p>
+            </div>
+        </div>
+    </main>
 </body>
 </html>
